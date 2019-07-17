4201c4ab6c9e45ee7dcd3038134470b06ba98acf
Kennashka DeSilva <mail@kennashka.com>
2019-07-17 11:20:38 -0400

    Create medianforevenarrays.js

---
+++ medianforevenarrays.js
@@ -0,0 +1,5 @@
+ if(concatarray.length % 2 === 0 ) {// array is even
+     var avg = (concatarray[(concatarray.length/2) - 1] + concatarray[concatarray.length/2]) /2;
+          return avg
+          
+          }
