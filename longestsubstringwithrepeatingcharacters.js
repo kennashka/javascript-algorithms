cac8ea940ace0b24a3c2d9cc7f0f8c0e82dd2377
Kennashka DeSilva <mail@kennashka.com>
2019-07-17 00:19:36 -0400

    Create longestsubstringwithrepeatingcharacters.js

---
+++ longestsubstringwithrepeatingcharacters.js
@@ -0,0 +1,15 @@
+var lengthOfLongestSubstring = function(s) {
+  let stringList = []
+  let maxLength = 0
+  
+  for(let i = 0; i < s.length; i++) {
+    if(stringList.includes(s[i])) {
+      const charToRemove = stringList.indexOf(s[i]) + 1
+      stringList = stringList.slice(charToRemove)
+    } 
+    stringList.push(s[i])
+    maxLength = Math.max(maxLength, stringList.length)
+  }
+  
+  return maxLength
+};
