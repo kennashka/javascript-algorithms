a4844f59f2197fdc3dbbe7b99d90bb184f17d03c
Kennashka DeSilva <mail@kennashka.com>
2019-07-17 23:51:40 -0400

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,1 +1,10 @@
 reverse = (str) => { return str.split("").reverse().join(""); }
+
+
+// another solution 
+const array = ['blastoff', 1, 2, 3];
+
+for (let index = array.length - 1; index >= 0; index--) {
+  const element = array[index];
+  console.log(element);
+}
