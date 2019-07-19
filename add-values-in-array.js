fa40d6bb0f24a15fd267ddd0512bfec34956ca83
Kennashka DeSilva <mail@kennashka.com>
2019-07-19 01:21:53 -0400

    Update add-values-in-array.js

--- add-values-in-array.js
+++ add-values-in-array.js
@@ -1,4 +1,8 @@
-function simpleArraySum(ar) {
+// prints the sum of numbers an array
+
+const simpleArraySum =(ar) =>{
     return ar.reduce((total, vote) => total + vote, 0);
 
+  
 }
+simpleArraySum([1, 2, 3, 4])
