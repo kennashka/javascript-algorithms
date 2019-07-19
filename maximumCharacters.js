bbc9d9a35053fa81b015ec5a6c2292ea560bdfb9
Kennashka DeSilva <mail@kennashka.com>
2019-07-18 22:23:53 -0400

    Update maximumCharacters.js

--- maximumCharacters.js
+++ maximumCharacters.js
@@ -1,13 +1,18 @@
 function MaxCharacter(str){ 
-let charMap = { };
+let charMap = {};
 str.split('').forEach(function(char){  
 if(charMap[char]) { 
 charMap[char]++;
 }
 else { 
-charMap[char]=1; 
+charMap[char] = 1; 
 }  
 });
+//console.log(charMap)
+
+for(let char in charMap){
+  
+}
 }
 
 MaxCharacter('blahya');
