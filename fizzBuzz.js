efda8a8bfdec4efe6c108bb1c95f4f028535541e
Kennashka DeSilva <mail@kennashka.com>
2019-07-20 14:56:18 -0400

    Update fizzBuzz.js

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -22,3 +22,21 @@ const fizzBuzz = () =>{
   }
 
 fizzBuzz()
+
+// another solution for returning items in an array
+var fizzBuzz = function(n) {
+    let res = [];
+    for(let i = 1; i <= n; i ++){
+        if(i % 15 === 0){
+            res.push("FizzBuzz");
+        }
+        else if(i % 3 === 0){
+            res.push("Fizz");
+        }
+        else if(i % 5 === 0){
+            res.push("Buzz");
+        }
+        else res.push(String(i));
+    }
+    return res;
+};
